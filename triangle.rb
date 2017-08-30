6b36db1c517810181e25db2c4e1c6e0cb7f97427
Chris Cadden <christopher.cadden@yale.edu>
2017-08-30 15:24:33 -0400

    finish more exercises

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides = [a, b, c]
+  raise TriangleError unless is_triangle(sides)
+  return which_triange(sides)
+end
+
+def which_triange(sides)
+  case sides.uniq.length
+  when 3
+    return :scalene
+  when 2
+    return :isosceles
+  when 1
+    return :equilateral
+  end
+end
+
+def is_triangle(sides)
+  return false if sides.select{ |side| side <= 0 }.length > 0
+  return false if sides[0] + sides[1] <= sides[2]
+  return false if sides[1] + sides[2] <= sides[0]
+  return false if sides[2] + sides[0] <= sides[1]
+  return true
 end
 
 # Error class used in part 2.  No need to change this code.
